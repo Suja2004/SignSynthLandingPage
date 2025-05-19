310675442a432aeab40304b9c4c316a43139d5a6
suja2004 <sujan.22cs164@sode-edu.in>
2025-05-19 22:31:06 +0530

    fixes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,7 @@
-
-
 function Footer() {
     return (
         <footer className="footer">
-            <h2>Team 16 &copy;2025</h2>
+            <h2>Team 16 &copy;2025 All rights reserved.</h2>
         </footer>
     )
 }
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,10 +3,13 @@ function Home() {
 
     return (
         <section id='home' className="home section">
-            <h1 >Al-Powered Sign Language Translation for Digital Medias</h1>
-            <p className='zoom'>Bridging the communication gap between the spoken world and the silent one.</p>
-            <p className='zoom'> SignSynth uses AI to translate speech and text into sign language in real-time making digital media accessible for all.</p>
-            <a href='https://github.com/Suja2004/ASR/releases/download/v0.1.0/SignSynth.zip' target='_blank' download={true}>  <button className='btn updown'>Install for Windows</button></a>
+            <div className="home-container">
+
+                <h1 >Al-Powered Sign Language Translation for Digital Medias</h1>
+                <p className='zoom'>Bridging the communication gap between the spoken world and the silent one.</p>
+                <p className='zoom'> SignSynth uses AI to translate speech and text into sign language in real-time making digital media accessible for all.</p>
+                <a href='https://github.com/Suja2004/ASR/releases/download/v0.1.0/SignSynth.zip' target='_blank' download={true}>  <button className='btn updown'>Install for Windows</button></a>
+            </div>
         </section>
     )
 }

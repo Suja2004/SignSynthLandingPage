ee7540b550e274d3423f3f9b31d117b2c2b8d98d
Sujan Kumar K <sujan.22cs164@sode-edu.in>
2025-11-28 20:53:54 +0530

    Update download link for SignSynth installer

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
                 <h1 >Al-Powered Sign Language Translation for Digital Medias</h1>
                 <p className='zoom'>Bridging the communication gap between the spoken world and the silent one.</p>
                 <p className='zoom'> SignSynth uses AI to translate speech and text into sign language in real-time making digital media accessible for all.</p>
-                <a href='https://github.com/Suja2004/ASR/releases/download/v1.0.0/SignSynth.exe' target='_blank' download={true}>
+                <a href='https://github.com/Suja2004/ASR/releases/download/v1.0.0/SignSynth-Installer.exe' target='_blank' download={true}>
                     <button className='btn updown'>Install for Windows</button>
                 </a>
             </div>
